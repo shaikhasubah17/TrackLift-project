4f9148cfbae1a4ffeb637ac034402958ec9cec94
Shaikha Subah <shaikhama127@gmail.com>
2026-07-16 14:26:58 +0300

    create workout model

---
+++ models/Workouts.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const workoutSchema = new mongoose.Schema({
+  workoutName: {
+    type: String,
+    required: true
+  },
+  workoutEquipment: {
+    type: String,
+    required: true
+  },
+  workoutDuration: {
+    type: String,
+    required: true
+  }, 
+  date: { 
+    type: Date,
+    required: true
+  }, 
+  workoutRating: { 
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true
+  }
+
+}, {timestamps: true});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
